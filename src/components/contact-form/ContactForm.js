848feb53a152271dbade81c5806fa69c5456bcdc
AngelBrd <angel.brand069@gmail.com>
2024-05-16 17:47:29 -0600

    Add correct submit function and error toasts

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 import './ContactForm.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ContactForm({ onClose }) {
   const [name, setName] = useState('');
   const [cel, setCel] = useState('');
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
-  const [error, setError] = useState('');
 
   const handleSubmit = (event) => {
     // Validación personalizada antes de enviar el formulario
     if (name.trim() === '' || cel.trim() === '' || date.trim() === '' || time.trim() === '') {
-      setError('Por favor, llene todos los campos antes de enviar el formulario.');
+      toast.error('Por favor, llene todos los campos antes de enviar el formulario.');
       event.preventDefault(); // Evita que el formulario se envíe si hay campos vacíos
       return;
     }
 
     // Validar el número de celular
     if (cel.length !== 10) {
-      setError('El número de celular debe tener 10 dígitos.');
+      toast.error('El número de celular debe tener 10 dígitos.');
       event.preventDefault(); // Evita que el formulario se envíe si el número de celular no tiene 10 dígitos
       return;
     }
@@ -30,7 +31,6 @@ function ContactForm({ onClose }) {
         <button className="close-button" onClick={onClose}>Cerrar</button>
       </div>
       <h2>Agenda tu cita</h2>
-      {error && <div className="error-toast">{error}</div>}
       <form onSubmit={handleSubmit} action='https://formsubmit.co/angel.brand069@gmail.com' method='POST'>
         <label htmlFor="name">Nombre:</label>
         <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required/>
@@ -54,6 +54,7 @@ function ContactForm({ onClose }) {
           <button type="submit" className='send-button'>Enviar</button>
         </div>
       </form>
+      <ToastContainer /> {/* Componente ToastContainer de react-toastify */}
     </div>
   );
 }
