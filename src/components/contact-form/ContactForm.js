0cae97ef11b46be34dfe546db78dffb8e8080cea
AngelBrd <angel.brand069@gmail.com>
2024-05-16 17:36:00 -0600

    Add correct submit function to send contact form

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './ContactForm.css';
-import axios from 'axios';
 
 function ContactForm({ onClose }) {
   const [name, setName] = useState('');
@@ -9,35 +8,21 @@ function ContactForm({ onClose }) {
   const [time, setTime] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    
-    // Verificar si los campos están llenos
+  const handleSubmit = (event) => {
+    // Validación personalizada antes de enviar el formulario
     if (name.trim() === '' || cel.trim() === '' || date.trim() === '' || time.trim() === '') {
       setError('Por favor, llene todos los campos antes de enviar el formulario.');
-      return; // Evita enviar el formulario si algún campo está vacío
+      event.preventDefault(); // Evita que el formulario se envíe si hay campos vacíos
+      return;
     }
 
     // Validar el número de celular
     if (cel.length !== 10) {
       setError('El número de celular debe tener 10 dígitos.');
+      event.preventDefault(); // Evita que el formulario se envíe si el número de celular no tiene 10 dígitos
       return;
     }
-    
-    try {
-      const response = await axios.post('https://formsubmit.co/angel.brand069@gmail.com', {
-        name,
-        cel,
-        date,
-        time
-      });
-      console.log(response.data); // Puedes manejar la respuesta aquí si lo deseas
-      onClose(); // Cierra la modal después de que se haya enviado el formulario
-    } catch (error) {
-      console.error('Error al enviar el formulario:', error);
-      // Aquí puedes manejar cualquier error que ocurra durante el envío del formulario
-    }
-  };  
+  };
 
   return (
     <div className="contact-form">
@@ -46,7 +31,7 @@ function ContactForm({ onClose }) {
       </div>
       <h2>Agenda tu cita</h2>
       {error && <div className="error-toast">{error}</div>}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} action='https://formsubmit.co/angel.brand069@gmail.com' method='POST'>
         <label htmlFor="name">Nombre:</label>
         <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required/>
 
